16a3fea1d71190ac8b352453abbe1c216de08baf
Luo Boming <fulhelm.gadgetter@gmail.com>
2024-01-08 10:51:44 +0900

    Update union_find

--- lib/union_find.hpp
+++ lib/union_find.hpp
@@ -3,24 +3,23 @@ class UnionFind
 {
 public:
    explicit UnionFind(size_t n)
-       : m_parents(n), m_sizes(n, 1), m_uniq_count(n)
+       : m_data(n, -1), m_uniq_count(n)
    {
-      std::iota(m_parents.begin(), m_parents.end(), 0);
    }
 
-   T find_root(T i)
+   T root(T i)
    {
-      if (m_parents[i] == i)
+      if (m_data[i] < 0)
       {
          return i;
       }
-      m_parents[i] = find_root(m_parents[i]);
-      return m_parents[i];
+      m_data[i] = root(m_data[i]);
+      return m_data[i];
    }
 
    void merge(int a, int b)
    {
-      a = find_root(a);
+      a = root(a);
       b = find_root(b);
       if (a == b)
       {
@@ -30,23 +29,21 @@ public:
       {
          swap(a, b);
       }
-      m_sizes[a] += m_sizes[b];
-      m_sizes[b] = 0;
-      m_parents[b] = a;
+      m_data[a] += m_data[b];
+      m_data[b] = a;
       m_uniq_count--;
    }
 
    int size(int i)
    {
-      return m_sizes[find_root(i)];
+      return -m_data[find_root(i)];
    }
 
-   bool is_connected(int a, int b)
+   bool is_merged(int a, int b)
    {
       return (find_root(a) == find_root(b));
    }
 
-   vector<T> m_parents;
-   vector<size_t> m_sizes;
+   vector<T> m_data;
    size_t m_uniq_count;
 };
