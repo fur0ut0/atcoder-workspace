5f1219663c17abbae2e19d2b75b015f5266cbf40
Luo Boming <fulhelm.gadgetter@gmail.com>
2024-02-15 09:39:51 +0900

    update prime library

--- lib/prime.hpp
+++ lib/prime.hpp
@@ -17,3 +17,13 @@ vector<T> prime_factorize(T n) {
    }
    return v;
 }
+
+template <typename T>
+bool is_prime(T n) {
+   for (T i = 2; i * i <= n; ++i) {
+      if (n % i == 0) {
+         return false;
+      }
+   }
+   return true;
+}
