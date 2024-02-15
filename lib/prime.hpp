7633ad18a1bd1fe2c807e3a576158c2aec107d51
Luo Boming <fulhelm.gadgetter@gmail.com>
2024-02-16 08:22:16 +0900

    add prime helper function

--- lib/prime.hpp
+++ lib/prime.hpp
@@ -27,3 +27,36 @@ bool is_prime(T n) {
    }
    return true;
 }
+
+template <typename T>
+vector<T> factorize(T n) {
+   vector<T> v;
+   for (T i = 2; i * i <= n; ++i) {
+      if (n % i == 0) {
+         v.push_back(i);
+         if (i * i < n) {
+            v.push_back(n / i);
+         }
+      }
+   }
+   return v;
+}
+
+template <typename T>
+T sieve(T n) {
+   constexpr size_t MAX_N = 1'000'000;
+   bool is_prime[MAX_N + 1];
+   fill(is_prime, is_prime + MAX_N + 1, true);
+   is_prime[0] = false;
+   is_prime[1] = false;
+   T size = 0;
+   for (T i = 2; i <= n; ++i) {
+      if (is_prime[i]) {
+         ++size;
+         for (T j = 2 * i; j <= n; j += i) {
+            is_prime[j] = false;
+         }
+      }
+   }
+   return size;
+}
