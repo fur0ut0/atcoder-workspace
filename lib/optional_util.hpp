2e5f4f1b2241ef39880c40df62efa93b43d399b9
Luo Boming <fulhelm.gadgetter@gmail.com>
2024-02-27 17:07:23 +0900

    delete unused library

--- lib/optional_util.hpp
+++
@@ -1,8 +0,0 @@
-template <typename T>
-bool optional_cmp(optional<T> a, optional<T> b) {
-   if (a.has_value() && b.has_value()) {
-      return a.value() < b.value()
-   } else {
-      return b.has_value();
-   }
-}
