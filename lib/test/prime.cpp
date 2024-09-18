b9febb61838c40cec008136c5437fc2e3cc881a9
Luo Boming <fulhelm.gadgetter@gmail.com>
2024-09-18 12:59:28 +0900

    fix: add namespace prefix

--- lib/test/prime.cpp
+++ lib/test/prime.cpp
@@ -6,7 +6,7 @@
 TEST(prime_factorize, 15) {
    using ll = std::int64_t;
    constexpr ll N = 15;
-   const auto expected = std::vector<pair<ll, ll>>{
+   const auto expected = std::vector<std::pair<ll, ll>>{
        {3, 1},
        {5, 1},
    };
@@ -17,7 +17,7 @@ TEST(prime_factorize, 15) {
 TEST(prime_factorize, 5328972) {
    using ll = std::int64_t;
    constexpr ll N = 5328972;
-   const auto expected = std::vector<pair<ll, ll>>{
+   const auto expected = std::vector<std::pair<ll, ll>>{
        {2, 2},
        {3, 2},
        {11, 1},
